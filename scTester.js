81231c6949ce17a72f5d128372b6c090b2ba1d05
paulwilcox <t78t78@gmail.com>
2020-01-09 13:40:58 -0700

    Using __dirname in scTester.js

--- scTester.js
+++ scTester.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 module.exports = function (config) {
 
     let ts = fs
-        .readFileSync('./scTesterTemplate.js').toString()
+        .readFileSync(__dirname + '/scTesterTemplate.js').toString()
         .replace('__testDirectory__', config.testDirectory)
         .replace('__port__', config.port)
         .replace('__serverImports__', config.serverImports)
