f12dfea7be40bbc78b17ae926b249cd9de9489fc
paulwilcox <t78t78@gmail.com>
2020-01-19 10:32:35 -0700

    Feedback for scTester.js

--- scTester.js
+++ scTester.js
@@ -2,17 +2,24 @@ let fs = require('fs');
 
 module.exports = function (config) {
 
+    let scTesterTemplate = __dirname + '/scTesterTemplate.js';
+    let scTesterServer = './scTesterServer.js';
+    
+    console.log(`Creating ${scTesterServer}`);
+    console.log(`  from ${scTesterTemplate}`)
+
     let ts = fs
-        .readFileSync(__dirname + '/scTesterTemplate.js').toString()
+        .readFileSync(scTesterTemplate).toString()
         .replace('__testDirectory__', config.testDirectory)
         .replace('__port__', config.port)
         .replace('__serverImports__', config.serverImports)
         .replace('__clientImports__', config.clientImports);
 
-    let writer = fs.createWriteStream('./scTesterServer.js');
+    let writer = fs.createWriteStream(scTesterServer);
     writer.write(ts);
-    writer.end(() => require('./scTesterServer.js'));
+    writer.end(() => require(scTesterServer));
 
 }
+
 
 
